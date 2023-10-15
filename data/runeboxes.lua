a1b1d3535df6cc70e2660350b08b293559555fa9
Anthony Oakley <agoakley@gmail.com>
2023-10-15 11:01:11 +0100

    runeboxes

--- data/runeboxes.lua
+++ data/runeboxes.lua
@@ -23,7 +23,7 @@ local RUNEBOX_FRAGMENTS = {
     [167305] = {cid = 8043, total = 51, fragments = {151939, 167303}}, -- Timbercrow Wanderer Costume
     [171472] = {cid = 8198, total = 10, fragments = {171469}}, -- Daggerfall Breton Terrier Pet
     [183195] = {cid = 9718, total = 31, fragments = {138783, 183194}}, -- Siegestomper Emote
-    [171533] = {cid = 8655, total = 31, fragments = {151939, 171532}} -- Rage of the Reach Emote
+    [171533] = {cid = 8655, total = 31, fragments = {151939, 171532}}, -- Rage of the Reach Emote
 }
 
 local COLLECTIBLE_FRAGMENTS = {
@@ -34,7 +34,8 @@ local COLLECTIBLE_FRAGMENTS = {
     [9389] = 10, -- Witch-Tamed Bear-Dog
     [9523] = 50, -- Replica Zenithar Adytum Gate
     [9797] = 50, -- Coral Haj Mota (25 Lures and 25 Decoys)
-    [10235] = 5 -- Cadwell's Surprise Box
+    [10235] = 5, -- Cadwell's Surprise Box
+    [10912] = 50 -- Graht-Oak Squirrel
 }
 
 function BS.GetCollectibleId(bagId, slotIndex)
--- settings/settings.lua
+++ settings/settings.lua
@@ -1,7 +1,7 @@
 local BS = _G.BarSteward
 
 BS.LAM = _G.LibAddonMenu2
-BS.VERSION = "1.7.2"
+BS.VERSION = "1.7.3"
 
 local panel = {
     type = "panel",
